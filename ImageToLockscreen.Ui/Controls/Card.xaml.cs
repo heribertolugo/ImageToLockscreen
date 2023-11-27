5e6c4f5ed1f14ce65b2913e8891fae5d66d3636c
Heriberto Lugo <heribertolugo@live.com>
2023-11-27 18:42:02 -0500

    created Card control

---
+++ ImageToLockscreen.Ui/Controls/Card.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ImageToLockscreen.Ui.Controls
+{
+    /// <summary>
+    /// Interaction logic for Card.xaml
+    /// </summary>
+    public partial class Card : UserControl
+    {
+        public Card()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly DependencyProperty TitleTextProperty = DependencyProperty.Register("Title", typeof(string), typeof(UserControl));
+        public static readonly DependencyProperty TitleForegroundProperty = DependencyProperty.Register("TitleForeground", typeof(Brush), typeof(UserControl));
+        public static readonly DependencyProperty TitleBackgroundProperty = DependencyProperty.Register("TitleBackground", typeof(Brush), typeof(UserControl));
+        public static readonly DependencyProperty IconProperty = DependencyProperty.Register("Icon", typeof(ImageSource), typeof(UserControl));
+
+        public string Title
+        {
+            get
+            {
+                return (string)GetValue(TitleTextProperty);
+            }
+            set
+            {
+                SetValue(TitleTextProperty, value);
+            }
+        }
+        public Brush TitleForeground
+        {
+            get
+            {
+                return (Brush)GetValue(TitleForegroundProperty);
+            }
+            set
+            {
+                SetValue(TitleForegroundProperty, value);
+            }
+        }
+        public Brush TitleBackground
+        {
+            get
+            {
+                return (Brush)GetValue(TitleBackgroundProperty);
+            }
+            set
+            {
+                SetValue(TitleBackgroundProperty, value);
+            }
+        }
+        public ImageSource Icon
+        {
+            get
+            {
+                return (ImageSource)GetValue(IconProperty);
+            }
+            set
+            {
+                SetValue(IconProperty, value);
+            }
+        }
+    }
+}
