4de8012ffb74c29b5adcbb8a8fed2816540a3cfb
Heriberto Lugo <heribertolugo@live.com>
2023-11-29 20:34:30 -0500

    added HintColor property

--- ImageToLockscreen.Ui/Controls/DirectoryPathControl.xaml.cs
+++ ImageToLockscreen.Ui/Controls/DirectoryPathControl.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace ImageToLockscreen.Ui.Controls
 {
@@ -15,12 +16,15 @@ namespace ImageToLockscreen.Ui.Controls
             InitializeComponent();
             this.FolderDialogTitle = DirectoryPathControl._folderDialogTitle;
             this._folderBrowserDialog = new FolderBrowserDialog();
+            this.HintColor = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#212121"));
         }
 
 
         public static readonly DependencyProperty HintTextProperty = DependencyProperty.Register("Hint", typeof(string), typeof(UserControl));
         public static readonly DependencyProperty ValueTextProperty = DependencyProperty.Register("Value", typeof(string), typeof(UserControl));
         public static readonly DependencyProperty FolderDialogTitleProperty = DependencyProperty.Register("FolderDialogTitle", typeof(string), typeof(UserControl));
+        public static readonly DependencyProperty HintColorProperty = DependencyProperty.Register("HintColor", typeof(Brush), typeof(UserControl));
+        //public static readonly DependencyProperty SearchColorProperty = DependencyProperty.Register("SearchColor", typeof(Brush), typeof(UserControl));
 
         public string Hint
         {
@@ -36,6 +40,12 @@ namespace ImageToLockscreen.Ui.Controls
         {
             get => (string)GetValue(FolderDialogTitleProperty);
             set => SetValue(FolderDialogTitleProperty, value);
+        }
+
+        public Brush HintColor
+        {
+            get => (Brush)GetValue(HintColorProperty);
+            set => SetValue(HintColorProperty, value);
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
