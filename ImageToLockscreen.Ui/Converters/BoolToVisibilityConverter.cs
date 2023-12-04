347ddf86651e7356a5943e3fe43db9129f0315e7
Heriberto Lugo <heribertolugo@live.com>
2023-12-03 23:16:42 -0500

    added converter to convert bool to Visibility value

---
+++ ImageToLockscreen.Ui/Converters/BoolToVisibilityConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ImageToLockscreen.Ui.Converters
+{
+    public sealed class BoolToVisibilityConverter : IValueConverter
+    {
+        private static readonly IReadOnlyDictionary<object, string> Visibility = new Dictionary<object, string>()
+        {
+            {string.Empty, "Collapsed" },
+            {true, "Visible" },
+            {false, "Hidden" }
+        };
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return BoolToVisibilityConverter.Visibility[value ?? string.Empty];
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException("BoolToVisibilityConverter ConvertBack Not Implemented");
+        }
+    }
+}
