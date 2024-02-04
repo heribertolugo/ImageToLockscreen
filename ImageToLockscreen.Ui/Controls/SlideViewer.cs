98f4f556d99e25ceaf53584c48fd74dc78187789
Heriberto Lugo <26213368+heribertolugo@users.noreply.github.com>
2024-02-04 14:35:57 -0500

    added xaml and code file for SlideViwer custom control

---
+++ ImageToLockscreen.Ui/Controls/SlideViewer.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace ImageToLockscreen.Ui.Controls
+{
+    public class SlideViewer : UserControl
+    {
+        public static readonly DependencyProperty BorderColorProperty = DependencyProperty.Register("BorderColor", typeof(Brush), typeof(UserControl));
+
+        public Brush BorderColor
+        {
+            get { return (Brush)base.GetValue(BorderColorProperty); }
+            set { base.SetValue(BorderColorProperty, value); }
+        }
+    }
+}
