31d67977f02dcf12fa4e72c89b10899380160aee
Heriberto Lugo <26213368+heribertolugo@users.noreply.github.com>
2024-02-04 14:32:48 -0500

    added ViewModel to define common aspect ratios

---
+++ ImageToLockscreen.Ui/ViewModels/CommonAspectRatios.cs
@@ -0,0 +1,29 @@
+﻿using ImageToLockscreen.Ui.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ImageToLockscreen.Ui.ViewModels
+{
+    internal class CommonAspectRatios
+    {
+        private ObservableCollection<AspectRatio> _commonRatios;
+
+        public CommonAspectRatios()
+        {
+
+        }
+
+        private void init()
+        {
+            this._commonRatios = new ObservableCollection<AspectRatio>()
+            {
+                new AspectRatio(new Ratio(3,2), new Size(32,32), "Camera"),
+            };
+        }
+    }
+}
