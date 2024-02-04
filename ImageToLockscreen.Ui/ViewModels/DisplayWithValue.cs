017fbef39a79c7880cec9592d29d1074b0a2c2b0
Heriberto Lugo <26213368+heribertolugo@users.noreply.github.com>
2024-02-04 14:33:13 -0500

    added ViewModel to hold pair of values

---
+++ ImageToLockscreen.Ui/ViewModels/DisplayWithValue.cs
@@ -0,0 +1,29 @@
+﻿using ImageToLockscreen.Ui.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageToLockscreen.Ui.ViewModels
+{
+    public class DisplayWithValue : PropertyObservable
+    {
+        public DisplayWithValue() { }
+        public DisplayWithValue(string display, object value)
+        {
+            Display = display;
+            Value = value;
+        }
+        public string Display
+        {
+            get { return GetProperty<string>(); }
+            set { SetProperty(value); }
+        }
+        public object Value
+        {
+            get { return GetProperty<string>(); }
+            set { SetProperty(value); }
+        }
+    }
+}
