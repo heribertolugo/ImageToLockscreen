4ed5501078849f9b0ad86fa9dbb201f8b85cee07
Heriberto Lugo <26213368+heribertolugo@users.noreply.github.com>
2024-02-04 14:31:28 -0500

    added Ratio model

---
+++ ImageToLockscreen.Ui/Models/Ratio.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageToLockscreen.Ui.Models
+{
+    public struct Ratio
+    {
+        public Ratio(double width, double height) 
+        { 
+            this.Width = width;
+            this.Height = height;
+        }
+        public double Width { get; private set; }
+        public double Height { get; private set; }
+    }
+}
