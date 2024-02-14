85afe017b1d199865e0be4decff2748656c440d1
Heriberto Lugo <26213368+heribertolugo@users.noreply.github.com>
2024-02-13 20:05:28 -0500

    added common aspect ratios

--- ImageToLockscreen.Ui/ViewModels/CommonAspectRatios.cs
+++ ImageToLockscreen.Ui/ViewModels/CommonAspectRatios.cs
@@ -1,28 +1,41 @@
 ﻿using ImageToLockscreen.Ui.Models;
-using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace ImageToLockscreen.Ui.ViewModels
 {
     internal class CommonAspectRatios
     {
-        private ObservableCollection<AspectRatio> _commonRatios;
 
-        public CommonAspectRatios()
+        //public CommonAspectRatios()
+        //{
+
+        //}
+
+        public static ObservableCollection<AspectRatio> CommonRatios;
+        static CommonAspectRatios()
         {
-
-        }
-
-        private void init()
-        {
-            this._commonRatios = new ObservableCollection<AspectRatio>()
+            CommonAspectRatios.CommonRatios = new ObservableCollection<AspectRatio>()
             {
-                new AspectRatio(new Ratio(3,2), new Size(32,32), "Camera"),
+                new AspectRatio(ratio: new Ratio(3,2), bounds: new Size(32,32),description: "Camera"),
+                new AspectRatio(ratio: new Ratio(4,3), bounds: new Size(32,32),description: "SDTV / PC",
+                    sizes: new[] { new Size(2048, 1536), new Size(1600,1200), new Size(640,480), 
+                        new Size(800,600), new Size(1024,768) }),
+                new AspectRatio(ratio: new Ratio(5,4), bounds: new Size(32,32),description: "PC",
+                    sizes: new[] { new Size(1280,1024) }),
+                new AspectRatio(ratio: new Ratio(16,10), bounds: new Size(32,32),description: "Wide PC",
+                    sizes: new[] { new Size(1280,800), new Size(1440,900), new Size(1680,1050), 
+                        new Size(1920,1200), new Size(2560,1600) }),
+                new AspectRatio(ratio: new Ratio(16,9), bounds: new Size(32,32),description: "HDTV",
+                    sizes: new[] { new Size(640,360), new Size(1280,720), new Size(1536,864), 
+                        new Size(1600,900), new Size(1920,1080), new Size(2048,1152), new Size(2560,1440), 
+                        new Size(3840, 2160), new Size(1360,768)}),
+                new AspectRatio(ratio: new Ratio(1.85,1), bounds: new Size(32,32),description: "Cinema"),
+                new AspectRatio(ratio: new Ratio(2.35,1), bounds: new Size(32,32),description: "Cinemascope"),
+                new AspectRatio(ratio: new Ratio(9,16), bounds: new Size(32,32),description: "Mobile Vertical"),
+                new AspectRatio(ratio: new Ratio(1,1), bounds: new Size(32,32),description: "Pro"),
+                new AspectRatio(ratio: new Ratio(21,9), bounds: new Size(32,32),description: "UW", 
+                    sizes: new[] { new Size(3440,1440), new Size(2560,1080) })
             };
         }
     }
