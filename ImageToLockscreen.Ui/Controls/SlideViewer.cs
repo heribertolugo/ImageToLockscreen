ba58fb5d1853e41a8069462a72c67e8ece66a046
Heriberto Lugo <26213368+heribertolugo@users.noreply.github.com>
2024-02-13 20:01:00 -0500

    added items property

--- ImageToLockscreen.Ui/Controls/SlideViewer.cs
+++ ImageToLockscreen.Ui/Controls/SlideViewer.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using ImageToLockscreen.Ui.Core;
+using System.Collections.ObjectModel;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 
@@ -8,10 +10,46 @@ namespace ImageToLockscreen.Ui.Controls
     {
         public static readonly DependencyProperty BorderColorProperty = DependencyProperty.Register("BorderColor", typeof(Brush), typeof(UserControl));
 
+
+        private ObservableCollection<object> _items;
+        public SlideViewer()
+        {
+            this._items = new ObservableCollection<object>();
+        }
+
         public Brush BorderColor
         {
             get { return (Brush)base.GetValue(BorderColorProperty); }
             set { base.SetValue(BorderColorProperty, value); }
+        }
+
+        public ObservableCollection<object> Items
+        {
+            get { return this._items; }
+            private set { this._items = value; }
+        }
+    }
+
+    public class SlideViewerItem : PropertyObservable
+    {
+        //public static readonly DependencyProperty ImageProperty = DependencyProperty.Register("BorderColor", typeof(Brush), typeof(UserControl));
+
+        public Image Image 
+        {
+            get { return base.GetProperty<Image>(); } 
+            set { base.SetProperty(value); }
+        }
+
+        public string Text
+        {
+            get { return base.GetProperty<string>(); }
+            set { base.SetProperty(value); }
+        }
+
+        public object Value
+        {
+            get { return base.GetProperty<object>(); }
+            set { base.SetProperty(value); }
         }
     }
 }
