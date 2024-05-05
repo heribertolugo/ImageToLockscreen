cb90a82ec8ec8f9caeb75fb055921d4f1c5e46b9
Heriberto Lugo <26213368+heribertolugo@users.noreply.github.com>
2024-05-04 23:57:04 -0400

    added IsVisible property

--- ImageToLockscreen.Ui/ViewModels/DisplayWithValue.cs
+++ ImageToLockscreen.Ui/ViewModels/DisplayWithValue.cs
@@ -5,19 +5,25 @@ namespace ImageToLockscreen.Ui.ViewModels
     public class DisplayWithValue : PropertyObservable
     {
         public DisplayWithValue() { }
-        public DisplayWithValue(string display, object value)
+        public DisplayWithValue(string display, BackgroundFillImageOption value, bool isVisible)
         {
-            Display = display;
-            Value = value;
+            this.Display = display;
+            this.Value = value;
+            this.IsVisible = isVisible;
         }
         public string Display
         {
             get { return base.GetProperty<string>(); }
             set { base.SetProperty(value); }
         }
-        public object Value
+        public BackgroundFillImageOption Value
         {
-            get { return base.GetProperty<string>(); }
+            get { return base.GetProperty<BackgroundFillImageOption>(); }
+            set { base.SetProperty(value); }
+        }
+        public bool IsVisible
+        {
+            get { return base.GetProperty<bool>(getDefault:() => true); }
             set { base.SetProperty(value); }
         }
     }
