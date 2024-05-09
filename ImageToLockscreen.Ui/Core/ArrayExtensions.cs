686c558ef4a3b5aa8de9f60205ea509e4a7d7ad8
Heriberto Lugo <26213368+heribertolugo@users.noreply.github.com>
2024-05-09 00:24:52 -0400

    added merge array extension method

---
+++ ImageToLockscreen.Ui/Core/ArrayExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace ImageToLockscreen.Ui.Core
+{
+    internal static class ArrayExtensions
+    {
+        public static string[] Merge(this string[] array, string[] arr)
+        {
+            string[] newArray = new string[array.Length + arr.Length];
+
+            for (int i = 0; i < array.Length; i++)
+                newArray[i] = array[i];
+            for (int j = 0; j < arr.Length; j++)
+                newArray[array.Length + j] = arr[j];
+            return newArray;
+        }
+    }
+}
