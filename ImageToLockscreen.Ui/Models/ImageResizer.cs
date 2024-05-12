0152a59cc4ddb0fbae54f22c9b6660f12a459c60
Heriberto Lugo <26213368+heribertolugo@users.noreply.github.com>
2024-05-11 23:59:59 -0400

    implemented image conversion

--- ImageToLockscreen.Ui/Models/ImageResizer.cs
+++ ImageToLockscreen.Ui/Models/ImageResizer.cs
@@ -1,51 +1,116 @@
 ﻿using ImageToLockscreen.Ui.Core;
 using System;
-using System.Drawing;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace ImageToLockscreen.Ui.Models
 {
     internal class ImageResizerEventAgs : EventArgs
     {
-        public ImageResizerEventAgs(float progress, string filename)
+        public ImageResizerEventAgs(int progress, string filename, bool success)
         {
             this.Progress = progress;
             this.FileName = filename;
+            this.Success = success;
         }
-        public float Progress { get; private set; }
+        public int Progress { get; private set; }
         public string FileName { get; private set; }
+        public bool Success { get; private set; }
     }
 
-    internal class ImageResizer
+    internal sealed class ImageResizer
     {
         public event EventHandler<ImageResizerEventAgs> OnProgress;
+
+        private int _progress;
+        private float Total { get; set; } = 0;
+
         public async Task Resize(ImageResizerOptions options)
         {
-            // if directory ! exists
-            foreach(var directory in Directory.EnumerateFiles(options.InputDirectory))
-            {
+            if (!Directory.Exists(options.InputDirectory))
+                throw new DirectoryNotFoundException($"input directory not found: {options.InputDirectory}");
+            if (!Directory.Exists(options.OutputDirectory))
+                throw new DirectoryNotFoundException($"output directory not found: {options.OutputDirectory}");
 
+            string[] files = Directory.GetFiles(options.InputDirectory);
+            
+            this.Total = files.Length;
+
+            foreach (var filename in files)
+            {
+                await this.ProcessImage(filename, options.TargetRatio, options.OutputDirectory, options.BackgroundOption);
             }
         }
 
-        public double Progress { get; private set; }
-
-        private Image GetImage(string path)
+        public int Progress { get => Volatile.Read(ref this._progress); }
+        private async Task ProcessImage(string path, Ratio ratio, string targetDirectory, BackgroundOption backgroundOption)
         {
-            Image image = null;
+            string newFileName = Path.Combine(targetDirectory, Path.GetFileName(path)); 
+
+            if (!IsValidImageFile(path))
+            {
+                this.ProgressReached(new ImageResizerEventAgs((int)(Interlocked.Increment(ref this._progress) / this.Total * 100), newFileName, false));
+                return;
+            }
+
+            BitmapImage image = GetImage(path);
+
+            if (image is null)
+            {
+                this.ProgressReached(new ImageResizerEventAgs((int)(Interlocked.Increment(ref this._progress) / this.Total * 100), newFileName, false));
+                return;
+            }
+
+            if (backgroundOption.IsImage && backgroundOption.BackgroundImagePath is null)
+                backgroundOption = new BackgroundOption(path, backgroundOption.IsBlurred, backgroundOption.DpiX, backgroundOption.DpiY);
+
+            var newImage = await GenerateNewImage(image, ratio, backgroundOption);
+            image = null;
+
+            if (newImage is null)
+            {
+                this.ProgressReached(new ImageResizerEventAgs((int)(Interlocked.Increment(ref this._progress) / this.Total * 100), newFileName, false));
+                return;
+            }
+
+            BitmapEncoder encoder = GetImageEncoder(Path.GetExtension(path));
+
+            encoder.Frames.Add(BitmapFrame.Create(newImage));
+
+            using (var fileStream = new FileStream(newFileName, FileMode.Create))
+            {
+                encoder.Save(fileStream);
+            }
+
+            this.ProgressReached(new ImageResizerEventAgs((int)(Interlocked.Increment(ref this._progress) / this.Total * 100), newFileName, true));
+            GC.Collect();
+        }
+        private void ProgressReached(ImageResizerEventAgs e)
+        {
+            EventHandler<ImageResizerEventAgs> handler = this.OnProgress;
+            handler?.Invoke(this, e);
+        }
+        private BitmapImage GetImage(string path)
+        {
+            BitmapImage image = null;
             try
             {
-                image = Image.FromFile(path);
+                image = new BitmapImage(new Uri(path));
+                image.Freeze();
             }
-            catch (OutOfMemoryException ex)
+            catch (OutOfMemoryException)
             {
                 return null;
             }
+
             return image;
         }
-
         /// <summary>
         /// Reads the header of different image formats
         /// </summary>
@@ -59,7 +124,7 @@ namespace ImageToLockscreen.Ui.Models
 
             try
             {
-                using (System.IO.FileStream fs = new System.IO.FileStream(file, System.IO.FileMode.Open, System.IO.FileAccess.Read))
+                using (FileStream fs = new FileStream(file, FileMode.Open, FileAccess.Read))
                 {
                     if (fs.Length > buffer.Length)
                     {
@@ -71,76 +136,107 @@ namespace ImageToLockscreen.Ui.Models
                     fs.Close();
                 }
 
-                foreach(var fileType in AllowedFileTypes.All.OrderByDescending(f => f.Header))
+                foreach(var fileType in AllowedFileTypes.All.OrderByDescending(f => f.Header.Length))
                 {
                     if (buffer.StartsWith(fileType.Header) && (fileType.Tail.Length == 0 || bufferEnd.EndsWith(fileType.Tail)))
                         return true;
                 }
             }
-            catch (Exception ex) { }
+            catch (Exception) { }
 
             return false;
+        }
+        private Task<RenderTargetBitmap> GenerateNewImage(BitmapImage image, Ratio ratio, BackgroundOption backgroundOption)
+        {
+            Size canvasSize = CalcNewSizeFromRatio(ratio, new Size(image.PixelWidth, image.PixelHeight));
+            BitmapSource background = GetBackground(backgroundOption, canvasSize);
+            DrawingGroup group = new DrawingGroup();
+            Rect overlayRect = new Rect(
+                (background.PixelWidth - image.PixelWidth) / 2,
+                (background.PixelHeight - image.PixelHeight) / 2,
+                image.PixelWidth, image.PixelHeight);
 
+            RenderOptions.SetBitmapScalingMode(group, BitmapScalingMode.HighQuality);
+            group.Children.Add(new ImageDrawing(background, new Rect(canvasSize)));
+            group.Children.Add(new ImageDrawing(image, overlayRect));
 
-            //byte[] buffer = new byte[8];
-            //byte[] bufferEnd = new byte[2];
+            DrawingVisual drawingVisual = new DrawingVisual();
+            using (DrawingContext drawingContext = drawingVisual.RenderOpen())
+                drawingContext.DrawDrawing(group);
 
-            //var bmp = new byte[] { 0x42, 0x4D };               // BMP "BM"
-            //var gif87a = new byte[] { 0x47, 0x49, 0x46, 0x38, 0x37, 0x61 };     // "GIF87a"
-            //var gif89a = new byte[] { 0x47, 0x49, 0x46, 0x38, 0x39, 0x61 };     // "GIF89a"
-            //var png = new byte[] { 0x89, 0x50, 0x4e, 0x47, 0x0D, 0x0A, 0x1A, 0x0A };   // PNG "\x89PNG\x0D\0xA\0x1A\0x0A"
-            //var tiffI = new byte[] { 0x49, 0x49, 0x2A, 0x00 }; // TIFF II "II\x2A\x00"
-            //var tiffM = new byte[] { 0x4D, 0x4D, 0x00, 0x2A }; // TIFF MM "MM\x00\x2A"
-            //var jpeg = new byte[] { 0xFF, 0xD8, 0xFF };        // JPEG JFIF (SOI "\xFF\xD8" and half next marker xFF)
-            //var jpegEnd = new byte[] { 0xFF, 0xD9 };           // JPEG EOI "\xFF\xD9"
+            var resizedImage = new RenderTargetBitmap(background.PixelWidth, background.PixelHeight,
+                background.DpiX, background.DpiY,PixelFormats.Default);
+            resizedImage.Render(drawingVisual);
 
-            //try
-            //{
-            //    using (System.IO.FileStream fs = new System.IO.FileStream(file, System.IO.FileMode.Open, System.IO.FileAccess.Read))
-            //    {
-            //        if (fs.Length > buffer.Length)
-            //        {
-            //            fs.Read(buffer, 0, buffer.Length);
-            //            fs.Position = (int)fs.Length - bufferEnd.Length;
-            //            fs.Read(bufferEnd, 0, bufferEnd.Length);
-            //        }
-
-            //        fs.Close();
-            //    }
-
-            //    if (buffer.StartsWith(bmp) ||
-            //        buffer.StartsWith(gif87a) ||
-            //        buffer.StartsWith(gif89a) ||
-            //        buffer.StartsWith(png) ||
-            //        buffer.StartsWith(tiffI) ||
-            //        buffer.StartsWith(tiffM))
-            //    {
-            //        return true;
-            //    }
-
-            //    if (buffer.StartsWith(jpeg))
-            //    {
-            //        // Offset 0 (Two Bytes): JPEG SOI marker (FFD8 hex)
-            //        // Offest 1 (Two Bytes): Application segment (FF?? normally ??=E0)
-            //        // Trailer (Last Two Bytes): EOI marker FFD9 hex
-            //        if (bufferEnd.StartsWith(jpegEnd))
-            //        {
-            //            return true;
-            //        }
-            //    }
-            //}
-            //catch (Exception ex) { }
-
-            //return false;
+            return Task.FromResult(resizedImage);
         }
 
-    }
-
-    internal class ImageResizerHelper
-    {
-        public ImageResizerHelper()
+        private BitmapEncoder GetImageEncoder(string imageType)
         {
+            switch (imageType)
+            {
+                case ".bmp":
+                    return new BmpBitmapEncoder();
+                case ".gif":
+                    return new GifBitmapEncoder();
+                case ".png":
+                    return new PngBitmapEncoder();
+                case ".tiff":
+                    return new TiffBitmapEncoder();
+                case ".jpg":
+                case ".jpeg":
+                case ".jiff":
+                    return new JpegBitmapEncoder();
+                default: throw new ArgumentNullException();
+            }
+        }
 
+        private Size CalcNewSizeFromRatio(Ratio ratio, Size size)
+        {
+            bool isWidthChange = ratio.Width > ratio.Height;
+            double target = isWidthChange ? size.Height : size.Width;
+
+            double newValue = Math.Round(target/Math.Min(ratio.Height, ratio.Width), 0) * Math.Max(ratio.Width, ratio.Height);
+
+            System.Diagnostics.Debug.Assert(Math.Round(isWidthChange ? newValue/size.Height : size.Width/newValue, 1) == Math.Round(ratio.Width/ratio.Height, 1));
+            
+            return isWidthChange ? new Size(newValue, size.Height) : new Size(size.Width, newValue);
+        }
+
+        private BitmapSource GetBackground(BackgroundOption backgroundOption, Size size)
+        {
+            if (!backgroundOption.IsImage)
+            {
+                int stride = ImageHelper.GetStride((int)size.Width, (int)backgroundOption.DpiX);
+                BitmapPalette myPalette = new BitmapPalette(new List<Color>() { backgroundOption.BackgroundColor });
+                return BitmapSource.Create((int)size.Width, (int)size.Height, backgroundOption.DpiX, backgroundOption.DpiY, PixelFormats.Indexed1, myPalette, 
+                    new byte[(int)size.Height * stride], stride);
+            }
+
+            BitmapSource background = ResizeImage(GetImage(backgroundOption.BackgroundImagePath), size, backgroundOption.DpiX, backgroundOption.DpiY);
+
+            if (!backgroundOption.IsBlurred)
+                return background;
+
+            var bmp = ImageHelper.BitmapSourceToBitmap(background);
+            ImageHelper.Blur(ref bmp, 8);
+            BitmapSource bg = ImageHelper.BitmapToImagesource(bmp);
+            bmp.Dispose();
+            return bg;
+        }
+        private BitmapSource ResizeImage(ImageSource source, Size targetSize, double dpiX = 96, double dpiY = 96)
+        {
+            DrawingGroup group = new DrawingGroup();
+            RenderOptions.SetBitmapScalingMode(group, BitmapScalingMode.HighQuality);
+            group.Children.Add(new ImageDrawing(source, new Rect(targetSize)));
+            DrawingVisual drawingVisual = new DrawingVisual();
+            using (DrawingContext drawingContext = drawingVisual.RenderOpen())
+                drawingContext.DrawDrawing(group);
+            RenderTargetBitmap resizedImage = new RenderTargetBitmap((int)targetSize.Width, (int)targetSize.Height,
+                dpiX, dpiY, PixelFormats.Default);
+            resizedImage.Render(drawingVisual);
+            
+            return resizedImage;
         }
     }
 
@@ -152,9 +248,7 @@ namespace ImageToLockscreen.Ui.Models
             this.InputDirectory = inputDirectory;
             this.OutputDirectory = outputDirectory;
             this.TargetRatio = targetRatio;
-            this.BackgroundImagePath = backgroundImagePath;
-            this.IsBlurred = isBlurred;
-            this.BackgroundColor = Color.Transparent;
+            this.BackgroundOption = new BackgroundOption(backgroundImagePath, isBlurred);
         }
 
         public ImageResizerOptions(string inputDirectory, string outputDirectory, Ratio targetRatio, Color backgroundColor)
@@ -162,16 +256,40 @@ namespace ImageToLockscreen.Ui.Models
             this.InputDirectory = inputDirectory;
             this.OutputDirectory = outputDirectory;
             this.TargetRatio = targetRatio;
-            this.BackgroundColor = backgroundColor;
-            this.IsBlurred = false;
-            this.BackgroundImagePath = string.Empty;
+            this.BackgroundOption = new BackgroundOption(backgroundColor);
         }
 
         public string InputDirectory { get; private set; }
-        public string OutputDirectory { get; private set; }
-        public Ratio TargetRatio { get; private set; }
-        public bool IsBlurred { get; private set; }
-        public Color BackgroundColor { get; private set; }
-        public string BackgroundImagePath { get; private set; }
+        public string OutputDirectory { get; }
+        public Ratio TargetRatio { get; }
+        public BackgroundOption BackgroundOption { get; }
+    }
+
+    internal readonly struct BackgroundOption
+    {
+        public BackgroundOption(Color backgroundColor, double dpiX = 96, double dpiY = 96)
+        {
+            this.BackgroundColor = backgroundColor;
+            this.IsBlurred = false;
+            this.BackgroundImagePath = string.Empty;
+            this.IsImage = false;
+            this.DpiX = dpiX;
+            this.DpiY = dpiY;
+        }
+        public BackgroundOption(string backgroundImagePath, bool isBlurred = false, double dpiX = 96, double dpiY = 96)
+        {
+            this.BackgroundColor = Colors.Transparent;
+            this.IsBlurred = isBlurred;
+            this.BackgroundImagePath = backgroundImagePath;
+            this.IsImage = true;
+            this.DpiX = dpiX;
+            this.DpiY = dpiY;
+        }
+        public bool IsBlurred { get; }
+        public Color BackgroundColor { get; }
+        public string BackgroundImagePath { get; }
+        public bool IsImage { get; }
+        public double DpiX { get; }
+        public double DpiY { get; }
     }
 }
