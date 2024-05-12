0334290bc8a9cf3f99b65283dfa62ec6787ccf7e
Heriberto Lugo <26213368+heribertolugo@users.noreply.github.com>
2024-05-12 12:56:42 -0400

    version 1

--- ImageToLockscreen.Ui/Properties/AssemblyInfo.cs
+++ ImageToLockscreen.Ui/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ImageToLockscreen.Ui")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Fixes image ratio so that they do not get resized when using to display in Windows lockscreen")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Heriberto Lugo")]
 [assembly: AssemblyProduct("ImageToLockscreen.Ui")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
